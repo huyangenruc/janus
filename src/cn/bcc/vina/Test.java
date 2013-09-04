21f4561819b9a39dca14b2d2e371cd6cf92cb0ac
huyangen <huyangen@qq.com>
2013-09-04 13:31:36 +0800

    13.31

--- src/cn/bcc/vina/Test.java
+++ src/cn/bcc/vina/Test.java
@@ -19,9 +19,6 @@ public class Test {
         int bucket = Integer.parseInt(args[1]);
         int topK = 100;
 		job.startJob(jobPath, dataPath, jobID, test, bucket, topK);
-		
-		System.out.println("just a test");
-		
 	}
 }
 	
---
+++ src/cn/bcc/vina/VinaJni.java
@@ -0,0 +1,13 @@
+package cn.bcc.vina;
+
+public class VinaJni {
+	//String conf,protein,element;
+	public native String getVinaResult(String config,String ligand,String receptor,String seed);
+	static
+	{
+		System.loadLibrary("vinajni.so");
+	}
+	/*public String getJavaVinaResult(String config,String ligand,String receptor,int seed){
+		return config+ligand+ligand;
+	}*/
+}
